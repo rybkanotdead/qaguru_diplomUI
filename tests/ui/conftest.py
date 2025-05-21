66b72b8319b8fa682074c3bbdc2744ee7e27d199
Alina Oganina <ifoxspirit@yandex.ru>
2025-05-21 21:04:27 +0500

    для pydantic==1/10/9

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selene import browser
 from webdriver_manager.chrome import ChromeDriverManager
-import config
 import logging
+
+import config
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +17,7 @@ def browser_management():
     if config.settings.ENVIRONMENT == 'local':
         options = Options()
         options.add_argument('--window-size=1920,1080')
+        # options.add_argument('--headless')  # если нужно без UI
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
 
@@ -25,23 +27,26 @@ def browser_management():
 
     else:  # remote (Selenoid)
         caps = {
-            'browserName': 'chrome',
-            'browserVersion': 'latest',
-            'selenoid:options': {
-                'enableVNC': True,
-                'enableVideo': True,
+            "browserName": "chrome",
+            "browserVersion": "latest",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True,
             }
         }
+
         remote_url = f"http://{config.settings.SELENOID_LOGIN}:{config.settings.SELENOID_PASSWORD}@selenoid.autotests.cloud/wd/hub"
         logger.info(f"Remote Selenoid URL: {remote_url}")
 
         driver = webdriver.Remote(
             command_executor=remote_url,
-            capabilities=caps
+            options=Options(),  # пустые опции, если не нужно добавлять что-то конкретное
+            desired_capabilities=caps  # Важно: для backward compatibility
         )
         browser.config.driver = driver
 
     browser.config.timeout = 10
 
     yield
+
     browser.quit()
