d97d7b1f43193921641ebd2619dc569172944410
Alina Oganina <ifoxspirit@yandex.ru>
2025-05-21 21:10:55 +0500

    для pydantic==1/10/9

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,12 +1,11 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selene import browser
 from webdriver_manager.chrome import ChromeDriverManager
-import logging
-
 import config
+import logging
 
 logger = logging.getLogger(__name__)
 
@@ -17,7 +16,6 @@ def browser_management():
     if config.settings.ENVIRONMENT == 'local':
         options = Options()
         options.add_argument('--window-size=1920,1080')
-        # options.add_argument('--headless')  # если нужен без UI
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
 
@@ -26,15 +24,7 @@ def browser_management():
         browser.config.driver = driver
 
     else:  # remote (Selenoid)
-        options = Options()
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-infobars')
-        options.add_argument('--disable-popup-blocking')
-        options.add_argument('--disable-notifications')
-
-        # capabilities для Selenoid
-        capabilities = {
+        caps = {
             'browserName': 'chrome',
             'browserVersion': 'latest',
             'selenoid:options': {
@@ -42,22 +32,16 @@ def browser_management():
                 'enableVideo': True,
             }
         }
-
-        # Передаём capabilities через options.set_capability
-        for key, value in capabilities.items():
-            options.set_capability(key, value)
-
         remote_url = f"http://{config.settings.SELENOID_LOGIN}:{config.settings.SELENOID_PASSWORD}@selenoid.autotests.cloud/wd/hub"
         logger.info(f"Remote Selenoid URL: {remote_url}")
 
         driver = webdriver.Remote(
             command_executor=remote_url,
-            options=options
+            capabilities=caps
         )
         browser.config.driver = driver
 
     browser.config.timeout = 10
 
     yield
-
     browser.quit()
