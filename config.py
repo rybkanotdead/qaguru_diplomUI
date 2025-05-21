b1640d78b64ef0ddd61cbe86abeded28160f7df9
Alina Oganina <ifoxspirit@yandex.ru>
2025-05-21 20:23:40 +0500

    для pydantic==1/10/9

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 from typing import Literal
 
-import pydantic_settings
+from pydantic import BaseSettings
 
 BASE_DIR = os.path.dirname(__file__)
+ENV_PATH = os.path.join(BASE_DIR, '.env')
 
 EnvType = Literal['local', 'remote']
 
-
-class Settings(pydantic_settings.BaseSettings):
+class Settings(BaseSettings):
     ENVIRONMENT: EnvType = 'local'
     USER_LOGIN: str
     USER_PASSWORD: str
@@ -16,5 +16,7 @@ class Settings(pydantic_settings.BaseSettings):
     SELENOID_LOGIN: str
     SELENOID_PASSWORD: str
 
+    class Config:
+        env_file = ENV_PATH
 
-settings = Settings(_env_file=os.path.join(BASE_DIR, '.env'))
+settings = Settings()
